47ad58db2892e9fb99da9f6dd3cd6ace7b4638f9
RyKilleen <rykilleen@gmail.com>
2015-07-08 21:11:33 -0400

    Rearranging scss, starting to get into angular bits

--- assets/js/mainController.js
+++ assets/js/mainController.js
@@ -1,5 +1,8 @@
-app.controller('MainController', ['$scope', function($scope) { 
+app.controller('MainController', ['$scope', '$log', function($scope, $log) { 
   $scope.title = 'Test'; 
   $scope.promo = "Oh hey";
-  
+
+  angular.element(document).ready(function () {
+        $log.debug('Hello world!');
+    }, $log);
 }]);
