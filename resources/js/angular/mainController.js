384bbfe8b1284e35cd217906dae7c80d9e913c4f
RyKilleen <rykilleen@gmail.com>
2015-07-19 21:42:38 -0400

    Changed google call to https. added cleaner hover events

--- resources/js/angular/mainController.js
+++ resources/js/angular/mainController.js
@@ -4,24 +4,54 @@ app.controller('MainController', ['$scope', '$log', function($scope, $log) {
   	$scope.header.toHides = $scope.header.find('.to-hide');
   	$scope.header.logo = $('.header-logo');
 
+  	$scope.header.logo.hovered = false;
+
+  	$scope.header.hideHeader = function() {
+  		var s = $scope;
+
+  		if ($scope.header.logo.hovered === false) {
+  			$scope.header.toHides.each(function(){
+				this.style.width = "0px";
+			})	
+  		}  		
+  	};
+
+  	$scope.header.showHeader = function() {
+  		s.header.toHides.each(function(){
+			this.style.width = $(this).data('origWidth');
+		});
+  	}
+
+
+
+
   	angular.element(document).ready(function () {
   		var s = $scope;
+
+  		// Get the true width of the displayed elements and place it into data
  		s.header.toHides.each(function(){
- 			$(this).data('origWidth' , this.offsetWidth);
- 			
+ 			$(this).data('origWidth' , this.offsetWidth); 			
  			this.style.width = this.offsetWidth;
  		});
 
  		setTimeout(function() {
- 			$scope.header.toHides.each(function(){
- 				this.style.width = "0px";
- 			})
+ 			$scope.header.hideHeader();
  		}, 1000, $scope);
 
+
  		$scope.header.logo.on('mouseenter', function() {
- 			s.header.toHides.each(function(){
- 				this.style.width = $(this).data('origWidth');
- 			});
+
+ 			$scope.header.logo.hovered = true;
+ 			$scope.header.showHeader();
+ 		});
+
+ 		$scope.header.logo.on('mouseleave', function() {
+
+ 			$scope.header.logo.hovered = false;
+
+ 			setTimeout(function() {
+	 			$scope.header.hideHeader();
+	 		}, 1000, $scope);
  		})
 
     }, $log, $scope);
