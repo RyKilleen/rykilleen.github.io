6ad1095561fb79640f13e027379988ccc04b3f68
RyKilleen <rykilleen@gmail.com>
2018-05-12 14:27:06 -0400

    tweak styles on about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,15 +7,20 @@ class About extends Component {
       <div className="about-page">
         <section className="about-section">
           <h2>Present</h2>
-          <p>Ryan is a Software Developer at <a href="https://www.thinkcompany.com">Think Company</a></p>
-          <p>More Coming soon!</p>
+          <section className="about-position">
+            <h3 className="position-employer">Think Company</h3>
+            <h4 className="position-meta"><span className="position-meta-position">Software Developer</span>, <span className="position-meta-date">July 2016 - Present</span></h4>
+        
+            <p>Ryan is a Software Developer at <a href="https://www.thinkcompany.com">Think Company</a></p>
+            <p>More Coming soon!</p>
+          </section>
         </section>
         <hr />
         <section className="about-section">
           <h2>Recent Past</h2>
-          <section className="about-sub-section">
-            <h3 className="about-employer">Nimblelight</h3>
-            <h4 className="about-employed-date">Front End Developer, 2014 - 2016</h4>
+          <section className="about-position">
+            <h3 className="position-employer">Nimblelight</h3>
+            <h4 className="position-meta"><span className="position-meta-position">Front End Developer</span>, <span className="position-meta-date">Jan. 2014 - July 2016</span></h4>
             <p>Ryan previously worked with the wonderful folks at <a href="https://nimblelight.com">Nimblelight</a>, getting the opportunity to learn and grow rapidly while wearing many different hats (and petting so many dogs.)</p>
             <p>At Nimblelight, he was able cut his teeth on:</p>
             <ul>
